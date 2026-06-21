fd6c775d35e3e48f08934057e250b30885e62cfc
Vitoarieshub <ceisavitim@gmail.com>
2026-06-20 22:44:33 -0300

    Update MinimizeButton image asset ID

--- Script.lua
+++ Script.lua
@@ -43,7 +43,7 @@ MakeWindow({
 
 
 MinimizeButton({
-    Image = "rbxassetid://72235587208723",
+    Image = "rbxassetid://87226466962985",
     Size = {40, 40},
     Color = Color3.fromRGB(10, 10, 10),
     Corner = true,                       
